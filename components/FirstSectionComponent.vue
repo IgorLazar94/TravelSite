eaeb5e8bc1936ab7e7a8f1a74c1fc674d5a9e168
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-09-01 20:33:50 +0300

    [firstSectionComponent] end

--- components/FirstSectionComponent.vue
+++ components/FirstSectionComponent.vue
@@ -25,17 +25,34 @@
 <style lang="scss">
 .first-section {
   &__wrapper {
-    background-color: rgb(114, 105, 105);
+    background-color: #0c1e27;
   }
 
   &__main-flex {
     position: relative;
     display: flex;
     justify-content: space-between;
+    flex-direction: row;
+    max-height: 720px;
+    @media (max-width: 1140 + "px") {
+      max-height: 509px;
+    }
   }
 
   &__content {
     position: relative;
+    margin: 141px 0 0 150px;
+    max-width: 632px;
+    padding: 5px;
+    @media (max-width: 1140 + "px") {
+      margin: 50px 0 0 25px;
+    }
+    @media (max-width: 750 + "px") {
+      margin: 10px 0 0 5px;
+    }
+    @media (max-width: 680 + "px") {
+      margin: 50px 0 0 25px;
+    }
   }
 
   &__number-absolute {
@@ -47,6 +64,11 @@
     color: #fff;
     top: -100px;
     left: -120px;
+    @media (max-width: 1140 + "px") {
+      font-size: 120px;
+      top: -120px;
+      left: -5px;
+    }
   }
 
   &__before-name {
@@ -68,6 +90,10 @@
     text-transform: uppercase;
     color: #fbd784;
     margin-bottom: 27px;
+    @media (max-width: 750 + "px") {
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
   }
 
   &__title {
@@ -76,6 +102,11 @@
     line-height: 77px;
     color: white;
     margin-bottom: 27px;
+    @media (max-width: 990 + "px") {
+      font-size: 40px;
+      margin-bottom: 5px;
+      line-height: 40px;
+    }
   }
 
   &__text {
@@ -84,29 +115,39 @@
     line-height: 32px;
     color: #fff;
     margin-bottom: 27px;
+    @media (max-width: 990 + "px") {
+      font-size: 16px;
+    }
+    @media (max-width: 325 + "px") {
+      margin-bottom: 5px;
+      font-size: 14px;
+    }
   }
-  //  -------------------------------------------отобразить svg после ссылки --------------------------------------------
   &__link {
     font-size: 18px;
     line-height: 22px;
     a {
       position: relative;
-      // left: 20px;
       color: #fbd784;
       text-decoration: none;
-      // padding-right: 15px;
       &::after {
         margin-left: 16px;
         vertical-align: middle;
         content: url("../static/icons/arrow-right.svg");
         width: 10px;
         height: 10px;
-        // background-image: ;
       }
     }
   }
-  //  -------------------------------------------отобразить картинку --------------------------------------------
   &__image {
+    img {
+      @media (max-width: 1140 + "px") {
+        max-width: 400px;
+      }
+      @media (max-width: 680+ "px") {
+        display: none;
+      }
+    }
   }
 }
 </style>
